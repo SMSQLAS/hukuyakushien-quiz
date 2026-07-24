93cecdaae0345bcf3192643a0e14233cf2db096a
SMSQLAS <hikarichozai@shikoku-ms.com>
2026-07-24 11:33:41 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,3 @@
 // 前回の自家製剤加算クイズで使用していたGoogle Apps ScriptのURLです。
-const GAS_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbw0Bm2PK_xUGjLsNIvVciUYS4B6cDui4h07dJwGLoSUfjdCdy-MWVbyhpoxTrM3EhNU5w/exec";
+const GAS_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbxCGQdVIRdGYjocIAdfrMA_gUYtxovXCQBj4IDBKXuUZTP7BKx5z0YVRRHIheUsRTI/exec";
+
